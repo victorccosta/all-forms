5a80a886b59857035da0408473deda05603603f3
Victor <victor@intelmed.tech>
2023-07-07 02:06:38 -0300

    Form ok. Neef quick fix to match openehr std

--- src/App.svelte
+++ src/App.svelte
@@ -12,17 +12,10 @@
 <main class="p-5 md:p-12 flex flex-col gap-5">
   <div class="flex justify-between">
     <a class="flex gap-3" href="/">
-      <img src="medblocks-ui-logo.png" class="w-6 h-8" alt="" />
-      <h1 class="text-2xl font-medium text-gray-700">Medblocks Forms</h1>
+      <img src="logo192.png" class="w-8 h-8" alt="" />
+      <h1 class="text-2xl font-medium text-gray-700">IntelCare</h1>
     </a>
-    <sl-button
-      type="neutral"
-      href="https://github.com/medblocks/all-forms"
-      target="_blank"
-    >
-      <sl-icon slot="suffix" name="github" />
-      Github
-    </sl-button>
+    
   </div>
   <Router>
     <Route path="/"><Home /></Route>
--- src/lib/Repeatable.svelte
+++ src/lib/Repeatable.svelte
@@ -7,7 +7,7 @@
 	<slot i = {i}></slot>
 {/each}
 <div>
-    <sl-button type="neutral" class="mt-2" on:click={()=>{number++;}}><sl-icon slot="prefix" name="plus-square-fill"></sl-icon>Add {name}</sl-button>
+    <sl-button type="neutral" class="mt-2" on:click={()=>{number++;}}><sl-icon slot="prefix" name="plus-square-fill"></sl-icon> Add {name}</sl-button>
     {#if number > 1}
         <sl-button on:click={()=>{number--;}}><sl-icon slot="prefix" name="dash-square-fill"></sl-icon>Delete {name}</sl-button>
     {/if}
